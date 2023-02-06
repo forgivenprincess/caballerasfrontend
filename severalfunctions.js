e916c0b2c6949e2eb94d5f6182a9a6010c868c29
forgivenprincess <amelia.psarmiento@gmail.com>
2023-02-06 18:52:57 +0100

    add several functions js

---
+++ severalfunctions.js
@@ -0,0 +1,47 @@
+function validatePassword(password) {
+    var pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return pattern.test(password);
+  }
+  var password = "Password1@";
+  if (validatePassword(password)) {
+    console.log("Password is valid");
+  } else {
+    console.log("Password is invalid");
+  }
+    
+
+  async function checkUsernameAndEmail(username, email) {
+    try {
+      const response = await fetch(`https://serverexample.com/api/check-username-and-email?username=${username}&email=${email}`);
+      const data = await response.json();
+      if (data.usernameExists || data.emailExists) {
+        return {
+          success: false,
+          message: data.usernameExists ? "Username already exists" : "Email already exists"
+        };
+      } else {
+        return {
+          success: true,
+          message: "Both username and email are available"
+        };
+      }
+    } catch (error) {
+      console.error(error);
+      return {
+        success: false,
+        message: "An error occurred while checking the availability of the username and email"
+      };
+    }
+  }
+  
+  const username = "newuser";
+  const email = "newuser@example.com";
+  checkUsernameAndEmail(username, email).then(result => {
+    if (result.success) {
+      console.log(result.message);
+      // Create the new user account here
+    } else {
+      console.error(result.message);
+    }
+  });
+  
