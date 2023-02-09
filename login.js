8fe4157f81162dfeab4c6547476e5466e7fc5363
forgivenprincess <amelia.psarmiento@gmail.com>
2023-02-09 17:16:27 +0100

    upload login.js and improve style sheet

---
+++ login.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("login-form");
+const usernameInput = document.getElementById("username-input");
+const passwordInput = document.getElementById("password-input");
+const errorMessage = document.getElementById("error-message");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  
+  if (username === "admin" && password === "password") {
+    window.location.href = "login.html";
+  } else {
+    errorMessage.textContent = "Invalid username or password.";
+  }
+});
